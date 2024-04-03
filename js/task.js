1b2982cdc5e142289ef67e49ccdda40033bee549
Katrinast <stehnovicekaterina@gmail.com>
2024-04-03 21:27:56 +0300

    add html

---
+++ js/task.js
@@ -0,0 +1,21 @@
+// Напиши функцію, яка перебирає масив логінів і перевіряє
+// чи є ім'я введене в інпут у цьому масиві і у разі, якщо є - виводить повідомлення "Доступ дозволено"
+// const logins = ["Mango", "Polly"];
+// const str = prompt(enter your login);
+
+// function checkLogin(logins, str) {
+//   return logins.includes(str) ? "Доступ дозволено" : "Доступ ne дозволено";
+  
+// }
+
+// console.log(checkLogin(logins, str));
+
+// Напишіть функцію min(a,b), яка повертає менше чисел a і b.
+
+function min(a, b) {
+  for (let i = a; i < b; i++) {
+    
+    
+  }
+  
+}
